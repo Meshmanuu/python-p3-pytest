670ed016a46be7fc66e63d135414fbebc9ca6840
Meshmanuu <meshanyangu@gmail.com>
2024-05-21 08:54:27 +0300

    lab done

--- lib/testing/test_string.py
+++ lib/testing/test_string.py
@@ -5,7 +5,8 @@ from string_functions import return_string, interpolate_string
 def test_return_string():
     '''in string_functions, function "return_string()" returns a variable of type str.'''
     assert type(return_string()) == str
-
+    pass
 def test_interpolate_string():
     '''in string_functions, function "interpolate_string()" takes a string and inserts it into another string.'''
     assert interpolate_string('Guido') == 'Hello, Guido!'
+    pass
